04baa9a7e9e481823f7677098b956f1c98ff063b
vaikundh <vaikundh.j@gmail.com>
2021-12-10 11:07:47 -0800

    More progress

--- bc-fa21/src/Pages/Portal/LoginOrSignup.tsx
+++ bc-fa21/src/Pages/Portal/LoginOrSignup.tsx
@@ -2,15 +2,35 @@ import React, { useState } from 'react';
 import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import { Box} from '@chakra-ui/react';
+import { animated, useTransition } from "react-spring";
 // import ApplicationPage from '../Application/ApplicationPage';
 
 // 2. Call `extendTheme` and pass your custom values
 
 function LoginOrSignup() {
   const [isLogin, updateIsLogin] = useState(true)
+
+  // const transitions = useTransition(isLogin, {
+  //   from: { position: 'absolute', opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  //   reverse: isLogin,
+  //   delay: 200,
+  //   config: config.molasses,
+  //   onRest: () => set(!toggle),
+  // })
+
+
     return (
       <Box>
         {isLogin? <Login updateIsLogin={updateIsLogin}/> : <Signup updateIsLogin={updateIsLogin}/>}
+        {/* {transitions.map(({ item, key, props }) =>
+        item ? (
+          <Login updateIsLogin={updateIsLogin}/>
+        ) : (
+          <Signup updateIsLogin={updateIsLogin}/>
+        )
+    )} */}
 
         {/* <ApplicationPage/> */}
       </Box>  
